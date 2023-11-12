947ae00faec89fd500325951d5a8bd6a5426430a
emmanuel <emmanuel_roman82231@elpoli.edu.co>
2023-11-12 15:52:06 -0500

    emma

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.menu2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.menu2 to javafx.fxml;
+    exports com.example.menu2;
+}
